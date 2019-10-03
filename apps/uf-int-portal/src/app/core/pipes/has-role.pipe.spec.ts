655f5c66cb68a0ef46e992f6992a42d68802a7d7
Christopher Moore <moorec2@findlay.edu>
2019-10-03 13:38:04 -0400

    Revert "added nrwl"
    This reverts commit a22c93206776aa68896c90a343840a4e8a6346bb.

--- apps/uf-int-portal/src/app/core/pipes/has-role.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HasRolePipe } from './has-role.pipe';
-
-describe('HasRolePipe', () => {
-  it('create an instance', () => {
-    const pipe = new HasRolePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
---
+++ src/app/shared/import-error-log/import-error-log.component.ts
@@ -0,0 +1,40 @@
+import { Component, ViewChild, Input } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { ILogDetail, LogDetailResponse } from 'src/api';
+
+@Component({
+  selector: 'uf-import-error-log',
+  templateUrl: './import-error-log.component.html',
+  styleUrls: ['./import-error-log.component.scss']
+})
+export class ImportErrorLogComponent {
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatTable, { static: false }) table: MatTable<ILogDetail>;
+  @Input() myService: any;
+  @Input() callParams: { mthd: string };
+  @Input() set exportDate(value: string) {
+    this._logDate = value;
+    this.onDateChanged();
+  }
+  ds = new MatTableDataSource<ILogDetail>();
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['Message', 'TimeStamp'];
+  private _logDate: string = '';
+
+  onDateChanged() {
+    this.myService[this.callParams.mthd](this._logDate).subscribe(
+      (data: LogDetailResponse) => {
+        this.ds.data = data.LogDetail;
+        this.ds.sort = this.sort;
+        this.ds.paginator = this.paginator;
+        this.table.dataSource = this.ds;
+      },
+      (error: string) => {
+        console.log(error);
+      }
+    );
+  }
+}
